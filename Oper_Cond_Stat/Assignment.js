eb75cf47c9d9c69de71671622382ddc58828e60a
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-30 00:01:29 +0530

    code

--- Oper_Cond_Stat/Assignment.js
+++ Oper_Cond_Stat/Assignment.js
@@ -6,3 +6,6 @@ console.log("After a += 4, a is", a); // Output: After a += 4, a is 9
 
 a -= 3; // Equivalent to a = a - 3
 console.log("After a -= 3, a is", a); // Output: After a -= 3, a is 6
+
+b *= 3; // Equivalent to b = b * 3
+console.log("After b *= 3, b is", b); // Output: After b *= 3, b is 6
