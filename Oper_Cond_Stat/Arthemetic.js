bb38ab7e517e7f46eb50c65f7cfbd9b158a2a468
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-29 11:47:49 +0530

    code

--- Oper_Cond_Stat/Arthemetic.js
+++ Oper_Cond_Stat/Arthemetic.js
@@ -1,8 +1,25 @@
 let a = 7
 let b = 3
-let c = 0
 
 console.log("Addition for a + b is", a + b) // Output: Addition for a + b is 10
+
 console.log("Subtraction for a - b is", a - b) // Output: Subtraction for a - b is 4        
+
 console.log("Multiplication for a * b is", a * b) // Output: Multiplication for a * b is 21
+
 console.log("Division for a / b is", a / b) // Output: Division for a / b is 2.3333333333333335 
+
+console.log("Modulus for a % b is", a % b) // Output: Modulus for a % b is 1 , it returns the remainder of the division
+
+console.log("Exponentiation for a ** b is", a ** b) // Output: Exponentiation for a ** b is 343, it returns a raised to the power of b
+
+
+
+// Uniary Operators
+
+console.log(a++) // Output: 7, it returns the value of a and then increments it by 1 , its equivalent to a = a + 1
+console.log(a) // Output: 8, it returns the value of a after incrementing it
+
+console.log(a--) // Output: 8, it returns the value of a and then decrements it by 1 , its equivalent to a = a - 1
+console.log(a) // Output: 7, it returns the value of a after decrementing it
+
