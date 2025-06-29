e763972e45b13b0abb71f3c194727dd56b8d6d2d
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-30 00:19:52 +0530

    code

--- Oper_Cond_Stat/Comparison.js
+++ Oper_Cond_Stat/Comparison.js
@@ -16,3 +16,6 @@ console.log("a === d is", a === d);  // Output: a === b is false, it checks if a
 console.log("a != b is", a != b);  // Output: a != b is true, it checks if a is not equal to b
 console.log("a != c is", a != c);  // Output: a != c is false, it checks if a is not equal to c
 console.log("a != d is", a != d);  // Output: a != d is false, it converts the string d to a number and checks if a is not equal to d
+
+// !== it checks if the values are not equal and also checks the data type
+console.log("a !== d is", a !== d);  // Output: a !== d is true, it checks if a is not equal to d and also checks the data type 
