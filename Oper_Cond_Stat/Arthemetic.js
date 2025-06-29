feaac7fab94c902a40a5c147abdabb21d5a16811
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-29 11:38:49 +0530

    code

--- Oper_Cond_Stat/Arthemetic.js
+++ Oper_Cond_Stat/Arthemetic.js
@@ -2,4 +2,7 @@ let a = 7
 let b = 3
 let c = 0
 
-console.log("Addition for a + b is", a + b)
+console.log("Addition for a + b is", a + b) // Output: Addition for a + b is 10
+console.log("Subtraction for a - b is", a - b) // Output: Subtraction for a - b is 4        
+console.log("Multiplication for a * b is", a * b) // Output: Multiplication for a * b is 21
+console.log("Division for a / b is", a / b) // Output: Division for a / b is 2.3333333333333335 
