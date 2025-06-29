39926ed280bf4b1e9ab05b650b5bd20853fa7f31
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-30 00:15:19 +0530

    code

---
+++ Oper_Cond_Stat/Comparison.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 2;
+let c = 5;
+let d = "5"
+
+console.log("a == b is ", a == b);  // Output: a == b is false, it checks if a is equal to b
+console.log("a == c is", a == c);  // Output: a == c is true, it checks if a is equal to c
+console.log("a == d is", a == d);  // Output: a == b is true, it converts the string d to a number and checks if a is equal to d
