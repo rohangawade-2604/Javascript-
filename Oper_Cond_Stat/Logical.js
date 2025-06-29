ca9f467b59b7faa7b90c405d89e3b212a02f0986
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-30 00:28:23 +0530

    code

---
+++ Oper_Cond_Stat/Logical.js
@@ -0,0 +1,9 @@
+let a = 6
+let b = 3;
+
+let cond1 = a > b;
+let cond2 = a === 6;
+let cond3 = a === 4;
+
+console.log("logical answer is ", cond1 && cond2); // Output: logical answer is cond1 && cond2 true, it checks if both conditions are true
+console.log("logical answer is ", cond1 && cond3); // Output: logical answer of cond1 && cond3 is false, it checks if both conditions are true
