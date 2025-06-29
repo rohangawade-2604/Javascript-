2f3bd28b17bd78b6ada6b1c5478a073b09d59ca9
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-30 01:09:43 +0530

    code

--- Oper_Cond_Stat/If-Else.js
+++ Oper_Cond_Stat/If-Else.js
@@ -1,9 +1,30 @@
 // Example 1
 
-let age = 20;
+// Write a program to calculate the age of the Student whether he is an adult or minor using if-else statement.
 
-if (age > 18) {
-    console.log("You are a adult.");
-} else {
-    console.log("You are an minor.");
+// let age = 20;
+
+// if (age > 18) {
+//     console.log("You are a adult.");
+// } else {
+//     console.log("You are an minor.");
+// }
+
+
+
+//Example 2
+
+// Write a program to implement the dark mode on a website using if-else statement.
+
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+    color = "black";
 }
+else{
+    color = "white";
+}
+
+
+
