034fe972fcadd41463b7b1e4906a003bdf31f7db
rohangawade-2604 <rohangawade2604@gmail.com>
2025-06-30 01:25:03 +0530

    nodthing

--- Oper_Cond_Stat/Else-if.js
+++ Oper_Cond_Stat/Else-if.js
@@ -1,1 +1,17 @@
 let mode = "dark";
+let color;
+
+if (mode === "dark"){
+    color = "black";
+}
+else if(mode === "pink"){
+    color = "pink";
+}
+else if(mode === "blue"){
+    color = "blue";
+}
+else{
+    color = "light"
+}
+
+console.log(color);
