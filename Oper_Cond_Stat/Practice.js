ab56a7a002ede4a5811666378e896ce63e55d2f5
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-02 21:27:20 +0530

    code

--- Oper_Cond_Stat/Practice.js
+++ Oper_Cond_Stat/Practice.js
@@ -1,1 +1,11 @@
 let number = prompt("Enter the number")
+
+if(number > 0){
+    console.log("the number is positive")
+}
+else if(number < 0) {
+    console.log("the number is negative")
+}
+else{
+    console.log("the number is zero")
+}
