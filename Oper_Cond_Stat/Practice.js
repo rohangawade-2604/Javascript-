23f30e95038281010664186bd0972dc74767aca4
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-02 21:38:38 +0530

    nothing

--- Oper_Cond_Stat/Practice.js
+++ Oper_Cond_Stat/Practice.js
@@ -11,7 +11,6 @@ else{
 }
 
 
-
 // Write a program to check whether a number is divisible by 5.
 
 let num = "10"
@@ -23,10 +22,28 @@ if(num % 5 === 0){
 }
 
 
+// Write a program to check whether a number is divisible by 10.
+
 let num1 = "52"
 
 if (num1 % 10 === 0){
     console.log("the number is multiply by 10")
 }else{
     console.log("invalid number")
+}
+
+
+let alpha = "a"
+
+let cond1 = alpha === "a"
+let cond2 = alpha === "e"
+let cond3 = alpha === "i"
+let cond4 = alpha === "o"
+let cond5 = alpha === "u"
+
+if(cond1 || cond2 || cond3 || cond4 || cond5){
+    console.log("The letter is a vowel.");
+}
+else{
+    console.log("The letter is a consonant.");
 }
