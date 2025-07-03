e89f8490ddb547fd96d9dae59b55f933491284e2
rohangawade-2604 <rohangawade2604@gmail.com>
2025-07-03 12:13:02 +0530

    code

--- Oper_Cond_Stat/Practice.js
+++ Oper_Cond_Stat/Practice.js
@@ -48,19 +48,19 @@ let marks = "150";
 if (marks >= 80 && marks <= 100){
     console.log("A+");
 }
-else if(marks >= 70){
+else if(marks >= 70 && marks <= 79){
     console.log("A");
 }
-else if(marks >= 60){
+else if(marks >= 60 && marks <= 69){
     console.log("B");
 }
-else if(marks >= 50){
+else if(marks >= 50 && marks <= 59){
     console.log("C");
 }
-else if(marks >= 40){
+else if(marks >= 40 && marks <= 49){
     console.log("D");
 }
-else if(marks >= 35){
+else if(marks >= 35 && marks <= 39){
     console.log("E");
 }
 else{
