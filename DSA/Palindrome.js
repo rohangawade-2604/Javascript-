b759586515d886fc89613a7d6c18fec69af3acb4
rohangawade-2604 <rohangawade2604@gmail.com>
2025-09-08 13:50:56 +0530

    code

--- DSA/Palindrome.js
+++ DSA/Palindrome.js
@@ -1,20 +1,28 @@
-const palindrome = 122;
+const palindrome = 121;
 
 const isPalindrome = () => {
 
-    let newArray = palindrome.toString().split("");
-    console.log(newArray);
+    let newArray = palindrome.toString().split("").map(Number);
+    // console.log(newArray);
+    let i = newArray.length-1;
+    let ans="";
 
-    let jhola = "";
-    for(let i=newArray.length-1; i>=0; i--){
-        jhola += newArray[i]
-        // jhola = jhola + newArray[i]
-        console.log(jhola)
+    while(i>=0){
+        ans += newArray[i];
+        i--
     }
-    
-    +jhola
+    console.log(ans)
 
-    if(jhola == palindrome){
+    // let jhola = "";
+    // for(let i=newArray.length-1; i>=0; i--){
+    //     jhola += newArray[i]
+    //     jhola = jhola + newArray[i]
+    //     console.log(jhola)
+    // }
+    
+    // +jhola
+
+    if(ans == palindrome){
         console.log("this is the palindrome part")
     }
     else{
