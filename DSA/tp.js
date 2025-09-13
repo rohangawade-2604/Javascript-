2ad0a21873358bcf071ef68f33f1afed2087a159
rohangawade-2604 <rohangawade2604@gmail.com>
2025-09-14 01:55:54 +0530

    code

---
+++ DSA/tp.js
@@ -0,0 +1,8 @@
+                                            // Creating an array
+                                            let names = ["Rohan", "Narsu", "Gawade"];
+
+                                            // Joining array into a single string
+                                            let fullName = names.join(" ");
+
+                                            // Printing the message
+                                            console.log("I am rngfires" + fullName + ", Welcome to my profile");
